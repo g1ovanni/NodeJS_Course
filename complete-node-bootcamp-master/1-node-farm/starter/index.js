d24ec7c5489a07dccae02686af8a290e3b9e7714
Athena <giovanni.gab@icloud.com>
2022-01-09 19:12:59 +0100

    Reading and Writing files - Asynchronously

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,8 +1,32 @@
 const fs = require("fs");
 
+// * //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// * Reading and Writing files - Synchronously
+/*
+
+// Is blocking, synchronous way
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
 const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File written!");
+*/
+
+// * //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// * Reading and Writing files - Asynchronously
+
+// Is Non-blocking, synchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR 💥");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been writeen 😃");
+      });
+    });
+  });
+}); // The third parameter is the call-back function, which will execute once the file reading in the first parameter is ready
+console.log("Will read file...");
