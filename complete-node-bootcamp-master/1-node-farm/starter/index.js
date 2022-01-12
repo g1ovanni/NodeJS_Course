a13a0a3b2644073bac3145860aa442a1d294143e
Athena <giovanni.gab@icloud.com>
2022-01-12 22:38:07 +0100

    HTML Templating: Filling the Templates - Fixing HTML file

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -33,7 +33,7 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
         console.log("Your file has been writeen 😃");
       });
     });
-  });
+});
 }); // The third parameter is the call-back function, which will execute once the file reading in the first parameter is ready
 console.log("Will read file...");
 
@@ -43,9 +43,42 @@ console.log("Will read file...");
 // * Creating a Simple Web Server
 // * Routing
 // * Building a (Very) Simple API
+// * HTML Templating: Building the Templates
+// * HTML Templating: Filling the Templates
 
+// SERVER
 // ? Building the API
 // Executed once at the start
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8"); // Read the data from the JSON file
 const dataObject = JSON.parse(data); // Parse the JSON file to an object
 
@@ -54,14 +87,29 @@ const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   // ? Routing
+  // Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
-  } else if (pathName === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObject
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    console.log(cardsHtml);
+
+    res.end(output);
+  }
+  // Product page
+  else if (pathName === "/product") {
     res.end("This is the PRODUCT");
-  } else if (pathName === "/api") {
+  }
+  // API
+  else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
-  } else {
+  }
+  // Not found
+  else {
     res.writeHead(404, {
       "Content-type": "text/html", // Se le dice al servidor que espere recibir contenido en texto o html
       "my-own-header": "Hello World",
@@ -73,12 +121,6 @@ const server = http.createServer((req, res) => {
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
 });
-
-// * //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// * HTML Templating: Building the Templates
-
-// * //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// * HTML Templating: Filling the Templates
 
 // * //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // * Parsing Variables from URLs
